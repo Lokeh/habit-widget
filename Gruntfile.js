eb9738677c7a6c1e3c3bb3ff363047e02fc41615
Will <will@willacton.com>
2015-06-12 14:31:51 -0700

    react, jsxhint and watch Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,19 +4,41 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['Gruntfile.js', './*.js', 'tests/*.js'],
+      gruntfile: {
+        src: 'Gruntfile.js',
+      },
+      all: ['./js/src/*.js' ],
       options: {
-        esnext: true,
-        node: true,
-        globals: {'require': true, 'module': true, 'console': true }
+        additionalSuffixes: ['.js']
+      }
+    },
+    react: {
+      files: {
+        expand: true,
+        cwd: 'js/src',
+        src: ['**/*.js'],
+        dest: 'js/build',
+        ext: '.js'
+      }
+    },
+    watch: {
+      gruntfile: {
+        'files': 'Gruntfile.js',
+        'tasks': ['jshint:gruntfile']
+      },
+      src: {
+        files: 'js/src/**/*.js',
+        tasks: ['default']
       }
     }
   });
 
-  // Load the plugin that provides the "jshint" task.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-jsxhint');
+  grunt.loadNpmTasks('grunt-react');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint:all', 'react']);
 
 };
