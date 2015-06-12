c307fe5e672555535adfef0895f472ce1dda196c
Will <will@willacton.com>
2015-06-12 13:36:55 -0700

    Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    jshint: {
+      all: ['Gruntfile.js', './*.js', 'tests/*.js'],
+      options: {
+        esnext: true,
+        node: true,
+        globals: {'require': true, 'module': true, 'console': true }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "jshint" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+
+};
