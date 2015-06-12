266c174072ae9243009de86f502e14b9bbad006c
Will <will@willacton.com>
2015-06-11 17:23:49 -0700

    get response.json and show all habits

--- js/build/habit.js
+++ js/build/habit.js
@@ -1,6 +1,34 @@
+var LatestHabit = React.createClass({displayName: "LatestHabit",
+	getInitialState: function () {
+		return { data: { todos: [], habits: [] } };
+	},
+	componentDidMount: function () {
+		console.log('mounted');
+		qwest.get(this.props.url)
+			.then(function (data) {
+				this.setState({ data: data });
+			}.bind(this))
+			.catch(function (e, data) {
+				console.log(e);
+				console.log(data);
+			})
+	},
+	render: function () {
+		var habits = this.state.data.habits.map(function (habit, i) {
+			return (
+				React.createElement("div", {className: "content", key: i}, 
+					habit.text
+				)
+			);
+		});
+
+		return (
+			React.createElement("div", null, habits)
+		);
+	}
+});
+
 React.render(
-	React.createElement("div", {className: "content"}, 
-		"My HabitRPG widget"
-	),
+	React.createElement(LatestHabit, {url: "response.json"}),
 	document.getElementById('habit-widget')
 );
--- js/src/habit.js
+++ js/src/habit.js
@@ -1,6 +1,34 @@
+var LatestHabit = React.createClass({
+	getInitialState: function () {
+		return { data: { todos: [], habits: [] } };
+	},
+	componentDidMount: function () {
+		console.log('mounted');
+		qwest.get(this.props.url)
+			.then(function (data) {
+				this.setState({ data: data });
+			}.bind(this))
+			.catch(function (e, data) {
+				console.log(e);
+				console.log(data);
+			})
+	},
+	render: function () {
+		var habits = this.state.data.habits.map(function (habit, i) {
+			return (
+				<div className="content" key={i}>
+					{habit.text}
+				</div>
+			);
+		});
+
+		return (
+			<div>{habits}</div>
+		);
+	}
+});
+
 React.render(
-	<div className="content">
-		My HabitRPG widget
-	</div>,
+	<LatestHabit url="response.json" />,
 	document.getElementById('habit-widget')
 );
