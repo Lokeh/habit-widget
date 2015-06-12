d06d9ba8159ec74a548101b81042b9d8e874ae73
Will <will@willacton.com>
2015-06-12 13:32:47 -0700

    lvl, hp, exp, mp

--- js/build/habit.js
+++ js/build/habit.js
@@ -1,6 +1,94 @@
+var byDate = function (el1, el2) {
+	var h1len = el1.history.length;
+	var h2len = el2.history.length;
+	if (h1len === 0 && h2len === 0) return 0;
+	if (h1len === 0) return 1;
+	if (h2len === 0) return -1;
+	var a = new Date(el1.history[h1len-1].date);
+	var b = new Date(el2.history[h2len-1].date);
+	return a>b ? -1 : a<b ? 1 : 0;
+};
+
 var LatestHabit = React.createClass({displayName: "LatestHabit",
+	render: function () {
+		var latestHabit = this.props.habits.concat(this.props.dailys).sort(byDate)[0];
+		if (!latestHabit) latestHabit = { text: 'Loading...' };
+		return (
+			React.createElement("div", null, 
+				"Latest task completed: ", React.createElement("em", null, latestHabit.text)
+			)
+		);
+	}
+});
+
+var HealthBar = React.createClass({displayName: "HealthBar",
+	render: function () {
+		return (
+			React.createElement("div", null, 
+				"HP: ", this.props.health.toFixed(2)
+			)
+		);
+	}
+});
+
+var ExpBar = React.createClass({displayName: "ExpBar",
+	render: function () {
+		return (
+			React.createElement("div", null, 
+				"EXP: ", this.props.exp.toFixed(2)
+			)
+		);
+	}
+});
+
+var ManaBar = React.createClass({displayName: "ManaBar",
+	render: function () {
+		return (
+			React.createElement("div", null, 
+				"MP: ", this.props.mana.toFixed(2)
+			)
+		);
+	}
+});
+
+var LevelIndicator = React.createClass({displayName: "LevelIndicator",
+	render: function () {
+		return (
+			React.createElement("div", null, 
+				"Lvl ", this.props.level
+			)
+		);
+	}
+});
+
+var ProfileName = React.createClass({displayName: "ProfileName",
+	render: function () {
+		return (
+			React.createElement("div", null, 
+				React.createElement("span", {className: "profile-name"}, this.props.name)
+			)
+		);
+	}
+});
+
+var Habit = React.createClass({displayName: "Habit",
 	getInitialState: function () {
-		return { data: { todos: [], habits: [] } };
+		return { 
+			data: {
+				todos: [],
+				habits: [],
+				dailys: [],
+				stats: {
+					hp: 0,
+					mp: 0,
+					exp: 0,
+					lvl: 0
+				},
+				profile: { 
+					name: null 
+				}
+			} 
+		};
 	},
 	componentDidMount: function () {
 		console.log('mounted');
@@ -11,24 +99,23 @@ var LatestHabit = React.createClass({displayName: "LatestHabit",
 			.catch(function (e, data) {
 				console.log(e);
 				console.log(data);
-			})
+			});
 	},
 	render: function () {
-		var habits = this.state.data.habits.map(function (habit, i) {
-			return (
-				React.createElement("div", {className: "content", key: i}, 
-					habit.text
-				)
-			);
-		});
-
 		return (
-			React.createElement("div", null, habits)
+			React.createElement("div", {className: "panel"}, 
+				React.createElement(ProfileName, {name: this.state.data.profile.name}), 
+				React.createElement(LevelIndicator, {level: this.state.data.stats.lvl}), 
+				React.createElement(HealthBar, {health: this.state.data.stats.hp}), 
+				React.createElement(ExpBar, {exp: this.state.data.stats.exp}), 
+				React.createElement(ManaBar, {mana: this.state.data.stats.mp}), 
+				React.createElement(LatestHabit, {habits: this.state.data.habits, dailys: this.state.data.dailys})
+			)
 		);
 	}
 });
 
 React.render(
-	React.createElement(LatestHabit, {url: "response.json"}),
+	React.createElement(Habit, {url: "response.json"}),
 	document.getElementById('habit-widget')
 );
--- js/src/habit.js
+++ js/src/habit.js
@@ -1,6 +1,94 @@
+var byDate = function (el1, el2) {
+	var h1len = el1.history.length;
+	var h2len = el2.history.length;
+	if (h1len === 0 && h2len === 0) return 0;
+	if (h1len === 0) return 1;
+	if (h2len === 0) return -1;
+	var a = new Date(el1.history[h1len-1].date);
+	var b = new Date(el2.history[h2len-1].date);
+	return a>b ? -1 : a<b ? 1 : 0;
+};
+
 var LatestHabit = React.createClass({
+	render: function () {
+		var latestHabit = this.props.habits.concat(this.props.dailys).sort(byDate)[0];
+		if (!latestHabit) latestHabit = { text: 'Loading...' };
+		return (
+			<div>
+				Latest task completed: <em>{latestHabit.text}</em>
+			</div>
+		);
+	}
+});
+
+var HealthBar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				HP: {this.props.health.toFixed(2)}
+			</div>
+		);
+	}
+});
+
+var ExpBar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				EXP: {this.props.exp.toFixed(2)}
+			</div>
+		);
+	}
+});
+
+var ManaBar = React.createClass({
+	render: function () {
+		return (
+			<div>
+				MP: {this.props.mana.toFixed(2)}
+			</div>
+		);
+	}
+});
+
+var LevelIndicator = React.createClass({
+	render: function () {
+		return (
+			<div>
+				Lvl {this.props.level}
+			</div>
+		);
+	}
+});
+
+var ProfileName = React.createClass({
+	render: function () {
+		return (
+			<div>
+				<span className="profile-name">{this.props.name}</span>
+			</div>
+		);
+	}
+});
+
+var Habit = React.createClass({
 	getInitialState: function () {
-		return { data: { todos: [], habits: [] } };
+		return { 
+			data: {
+				todos: [],
+				habits: [],
+				dailys: [],
+				stats: {
+					hp: 0,
+					mp: 0,
+					exp: 0,
+					lvl: 0
+				},
+				profile: { 
+					name: null 
+				}
+			} 
+		};
 	},
 	componentDidMount: function () {
 		console.log('mounted');
@@ -11,24 +99,23 @@ var LatestHabit = React.createClass({
 			.catch(function (e, data) {
 				console.log(e);
 				console.log(data);
-			})
+			});
 	},
 	render: function () {
-		var habits = this.state.data.habits.map(function (habit, i) {
-			return (
-				<div className="content" key={i}>
-					{habit.text}
-				</div>
-			);
-		});
-
 		return (
-			<div>{habits}</div>
+			<div className="panel">
+				<ProfileName name={this.state.data.profile.name} />
+				<LevelIndicator level={this.state.data.stats.lvl} />
+				<HealthBar health={this.state.data.stats.hp} />
+				<ExpBar exp={this.state.data.stats.exp} />
+				<ManaBar mana={this.state.data.stats.mp} />
+				<LatestHabit habits={this.state.data.habits} dailys={this.state.data.dailys} />
+			</div>
 		);
 	}
 });
 
 React.render(
-	<LatestHabit url="response.json" />,
+	<Habit url="response.json" />,
 	document.getElementById('habit-widget')
 );
