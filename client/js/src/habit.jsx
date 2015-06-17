f41b50da6a35354f9c4e93e8765fb8375fc6d370
Will <will@willacton.com>
2015-06-17 10:31:49 -0700

    moved files to client/

---
+++ client/js/src/habit.jsx
@@ -0,0 +1,135 @@
+
+// Create scope
+(function () {
+	const byDate = (el1, el2) => {
+		const h1len = el1.history.length;
+		const h2len = el2.history.length;
+		if (h1len === 0 && h2len === 0) return 0; // equal
+		if (h1len === 0) return 1; // el1 less
+		if (h2len === 0) return -1; // el2 less
+		const a = new Date(el1.history[h1len-1].date);
+		const b = new Date(el2.history[h2len-1].date);
+		return a>b ? -1 : a<b ? 1 : 0;
+	};
+
+	const LatestHabit = React.createClass({
+		render() {
+			const latestHabit = this.props.habits.concat(this.props.dailys).sort(byDate)[0] || { text: 'Loading...' };
+			// if (latestHabit) console.log(latestHabit.history[latestHabit.history.length-1].date);
+			return (
+				<div className="habit-list">
+					Latest task completed:
+					<div className="habit">
+						<span className="name">{latestHabit.text}</span>
+					</div>
+				</div>
+			);
+		}
+	});
+
+	const LevelIndicator = React.createClass({
+		render() { return (<span className="level">Lvl {this.props.level}</span>); }
+	});
+
+	const ProfileName = React.createClass({
+		render() { return (<span className="profile-name">{this.props.name}</span>); }
+	});
+
+	const GlyphLabel = React.createClass({
+		render() {
+			const glyphs = {
+				"hp": "fa fa-heart",
+				"exp": "fa fa-star",
+				"mp": "fa fa-fire"
+			};
+			const thisGlyph = glyphs[this.props.type];
+			return (
+				<div className="glyph-label">
+					<i className={thisGlyph}></i>
+				</div>
+			);
+		}
+	});
+
+	const StatBar = React.createClass({
+		render() {
+			const style = {width: Math.floor((this.props.statValue / this.props.max)*100) + '%' };
+			const classes = "meter " + this.props.name;
+			return (
+				<div>
+					<GlyphLabel type={this.props.name} />
+					<div className="stat-bar">
+						<div className={classes} style={style}></div>
+						<span className="text">
+							{this.props.statValue.toFixed()} / {this.props.max}
+						</span>
+					</div>
+				</div>
+			);
+		}
+	});
+
+	const Habit = React.createClass({
+		loadData(url, cb) {
+			console.log('loading...');
+			qwest.get(url, null, {
+				headers: {
+					'x-api-user': habitConfig.uid,
+					'x-api-key': habitConfig.key
+				},
+				cache: false
+			})
+			.then((data) => this.setState({ data: data }))
+			.catch((e, data) => {
+				console.log(e);
+				console.log(data);
+			});
+		},
+		getInitialState() {
+			return { 
+				data: {
+					todos: [],
+					habits: [],
+					dailys: [],
+					stats: {
+						hp: 0,
+						mp: 0,
+						exp: 0,
+						lvl: 0,
+						maxHealth: 0,
+						maxMP: 0,
+						toNextLevel: 0
+					},
+					profile: { 
+						name: null 
+					}
+				} 
+			};
+		},
+		componentDidMount() {
+			console.log('mounted');
+			this.loadData(this.props.url);
+			// setInterval(function () { this.loadData(this.props.url); }.bind(this), this.props.interval);
+		},
+		render() {
+			return (
+				<div className="panel">
+					<div className="profile-info">
+						<ProfileName name={this.state.data.profile.name} />
+						<LevelIndicator level={this.state.data.stats.lvl} />
+					</div>
+					<StatBar name="hp" statValue={this.state.data.stats.hp} max={this.state.data.stats.maxHealth} />
+					<StatBar name="exp" statValue={this.state.data.stats.exp} max={this.state.data.stats.toNextLevel} />
+					<StatBar name="mp" statValue={this.state.data.stats.mp} max={this.state.data.stats.maxMP} />
+					<LatestHabit habits={this.state.data.habits} dailys={this.state.data.dailys} />
+				</div>
+			);
+		}
+	});
+
+	// Render our parent component
+	React.render(
+		<Habit url="https://habitrpg.com:443/api/v2/user" />,
+		document.getElementById('habit-widget')
+	);
+})();
